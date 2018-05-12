396c0656cdb96183626efd710ec9c0c8b17b242f
Dan Poulsen <dangjogvara@hotmail>
2018-05-12 13:29:19 +0100

    Updated package.json and formatted files

--- mongodb-find.js
+++ mongodb-find.js
@@ -1,5 +1,5 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
   const db = client.db('TodoApp');
 
-  db.collection('Todos').find({_id}).toArray().then((docs) => {
+  db.collection('Todos').find({ _id }).toArray().then((docs) => {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
