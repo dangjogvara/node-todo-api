3b681b887a8aea7f7f2e952b60031d8b978dd0d6
Dan Poulsen <dangjogvara@hotmail>
2018-05-13 17:13:04 +0100

    PORT to port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('../models/todo');
 const {User} = require('../models/user')
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -64,6 +64,6 @@ app.post('/user', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
