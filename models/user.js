bf7b8f9b331745bfdb902610e9364b3a15915bb7
Dan Poulsen <dangjogvara@hotmail.com>
2018-05-21 17:54:00 +0100

    set email unique: true

--- models/user.js
+++ models/user.js
@@ -2,16 +2,12 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 var User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    minlength: 1
-  },
   email: {
     type: String,
     required: true,
     trim: true,
     minlength: 1,
+    unique: true,
     validate: {
       validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
