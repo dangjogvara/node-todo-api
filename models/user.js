9339b9164accf8416ac8ccfa4b824972fc5268a0
Dan Poulsen <dangjogvara@hotmail.com>
2018-05-21 15:20:00 +0100

    added validation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   name: {
@@ -10,8 +11,27 @@ var User = mongoose.model('User', {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {
