cbbeb0ddd030e232622de3168a44e983b4fd0d4a
Dan Poulsen <dangjogvara@hotmail.com>
2018-05-28 20:19:24 +0100

    POST /users/login

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
-const { User } = require('./models/user')
+const { User } = require('./models/user');
 
 const { authenticate } = require('./middleware/authenticate');
 
@@ -22,20 +22,26 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   let todo = new Todo({ text: req.body.text });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 // get todos
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({ todos });
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  Todo.find().then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 // get todo by ID
@@ -46,14 +52,16 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
-    if (!todo) {
-      res.status(404).send();
-    }
-    res.send({ todo });
-  }).catch((e) => {
-    res.status(400).send();
-  })
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 // delete todo
@@ -64,15 +72,16 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
-      res.status(404).send();
-    }
-    res.send(todo);
-  }).catch((e) => {
-    res.status(400).send();
-  });
-
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.send(todo);
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 // update todo
@@ -91,18 +100,22 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {
-    $set: body
-  }, { new: true }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-    res.send({ todo });
-
-  }).catch((e) => {
-    res.status(400).send();
-  });
-
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    { new: true }
+  )
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 // add user
@@ -110,18 +123,36 @@ app.post('/users', (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
   let user = new User(body);
 
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
-    res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
 });
 
 // Get user profile
 app.get('/users/profile', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
